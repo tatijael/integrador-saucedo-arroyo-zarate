28831e13c98492f4779efdd7af1df707ced0d703
Tatiana <tatijael@gmail.com>
2025-03-16 15:26:53 -0300

    replace multiple if-else conditions

--- book-api/server.js
+++ book-api/server.js
@@ -1,9 +1,7 @@
 const net = require('net');
-
 const bookController = require('./controllers/booksController');
 const authorController = require('./controllers/authorController');
 const pusblisherController = require('./controllers/publisherController');
-
 const { v4: uuidv4 } = require('uuid');
 
 function isJSON(str) {
@@ -15,59 +13,68 @@ const server = net.createServer((socket) => {
 
     socket.on('data', (data) => {
         const command = data.toString().trim();
-        if (command === 'GET BOOKS') {
-            const books = bookController.getAllBooks();
-            socket.write(books);
-        } else if (command === 'GET AUTHORS') {
-            const authors = authorController.getAllAuthors();
-            socket.write(authors);
-        } else if (command === 'GET PUBLISHERS') {
-            const publishers = pusblisherController.getAllPublishers();
-            socket.write(publishers);
-        } else if (command.startsWith('ADD BOOK')) {
-            const bookData = command.replace('ADD BOOK ', '');
-            if (isJSON(bookData)) {
-                const bookObject = JSON.parse(bookData);
-                bookObject.id = uuidv4();
-                const newBook = bookController.addBook(bookObject);
-                socket.write(JSON.stringify({ message: 'Libro agregado correctamente', book: newBook }));
-            }
-        } else if (command.startsWith('ADD AUTHOR')) {
-            const authorData = command.replace('ADD AUTHOR ', '');
-            if (isJSON(authorData)) {
-                const authorObject = JSON.parse(authorData);
-                authorObject.id = uuidv4();
-                const newAuthor = authorController.addAuthor(authorObject);
-                socket.write(JSON.stringify({ message: 'Autor agregado correctamente', author: newAuthor }));
-            }
-        } else if (command.startsWith('ADD PUBLISHER')) {
-            const publisherData = command.replace('ADD PUBLISHER ', '');
-            if (isJSON(publisherData)) {
-                const publisherObject = JSON.parse(publisherData);
-                publisherObject.id = uuidv4();
-                const newPublisher = pusblisherController.addPublisher(publisherObject);
-                socket.write(JSON.stringify({ message: 'Editorial agregada correctamente', publisher: newPublisher }));
-            }
-        } else if (command.startsWith('EDIT BOOK')) {
-            const bookData = command.replace('EDIT BOOK ', '');
-            if (isJSON(bookData)) {
-                const bookObject = JSON.parse(bookData);
-                const updatedBook = bookController.editBook(bookObject);
-                socket.write(JSON.stringify({  message: 'Libro actualizado correctamente',  book: updatedBook }));
-            } 
+        const [action, type, ...rest] = command.split(' ');
+        const jsonData = rest.join(' ');
+
+        switch(`${action} ${type}`) {
+            case 'GET BOOKS':
+                socket.write(bookController.getAllBooks());
+                break;
+            case 'GET AUTHORS':
+                socket.write(authorController.getAllAuthors());
+                break;
+            case 'GET PUBLISHERS':
+                socket.write(pusblisherController.getAllPublishers());
+                break;
+            case 'ADD BOOK':
+                if (isJSON(jsonData)) {
+                    const bookObject = JSON.parse(jsonData);
+                    bookObject.id = uuidv4();
+                    const newBook = bookController.addBook(bookObject);
+                    socket.write(JSON.stringify({ 
+                        message: 'Libro agregado correctamente', 
+                        book: newBook 
+                    }));
+                }
+                break;
+            case 'ADD AUTHOR':
+                if (isJSON(jsonData)) {
+                    const authorObject = JSON.parse(jsonData);
+                    authorObject.id = uuidv4();
+                    const newAuthor = authorController.addAuthor(authorObject);
+                    socket.write(JSON.stringify({ 
+                        message: 'Autor agregado correctamente', 
+                        author: newAuthor 
+                    }));
+                }
+                break;
+            case 'ADD PUBLISHER':
+                if (isJSON(jsonData)) {
+                    const publisherObject = JSON.parse(jsonData);
+                    publisherObject.id = uuidv4();
+                    const newPublisher = pusblisherController.addPublisher(publisherObject);
+                    socket.write(JSON.stringify({ 
+                        message: 'Editorial agregada correctamente', 
+                        publisher: newPublisher 
+                    }));
+                }
+                break;
+            case 'EDIT BOOK':
+                if (isJSON(jsonData)) {
+                    const bookObject = JSON.parse(jsonData);
+                    const updatedBook = bookController.editBook(bookObject);
+                    socket.write(JSON.stringify({ 
+                        message: 'Libro actualizado correctamente',  
+                        book: updatedBook 
+                    }));
+                }
+                break;
         }
     });
 
-    socket.on('end', () => {
-        console.log('Client disconnected');
-    });
-
-    socket.on('error', (error) => {
-        console.error('Socket error:', error);
-    });
+    socket.on('end', () => console.log('Client disconnected'));
+    socket.on('error', (error) => console.error('Socket error:', error));
 });
 
 const PORT = 8080;
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
